7a740f9c6a0a1bdef7d9ddc9ccc157c7f2f3ce48
thewillian <thewillian@hotmail.com>
2022-08-05 16:36:48 -0300

    first commit

---
+++ Desafio/main.mjs
@@ -0,0 +1,7 @@
+import { mostraIdade, mostraCidade, mostraHobby } from './funcoes.mjs';
+
+console.log(mostraIdade('Willian', 43));
+
+console.log(mostraCidade('Willian', 'São Paulo-SP'));
+
+console.log(mostraHobby('Willian', 'Natação'));
---
+++ Imports Dinâmicos/README.md
@@ -0,0 +1,5 @@
+# Imports Dinâmicos
+
+## Exercício
+
+Este exercício serve apenas como um exemplo de como utilizar módulos **por demanda**. Na nossa página HTML, ao clicar no botão, o fundo da página fica vermelho.
